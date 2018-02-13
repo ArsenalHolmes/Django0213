65dbb4e3b88acd921ba96fd68bfd11d5271fcee7
LH <909799631@qq.com>
2018-02-13 14:54:06 +0800

    修改模版前备份

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
+
+def index(request):
+    '''
+    return render(request, 'blog/index.html', context={
+                      'title': '我的博客首页',
+                      'welcome': '欢迎访问我的博客首页'
+                  })
+    '''
+
+    post_list = Post.objects.all().order_by('-created_time')#order_by 排序  依据是created_time 默认从远到近 加“-” 倒置
+    return render(request, 'blog/index.html', context={'post_list': post_list})
